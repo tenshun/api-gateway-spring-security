2228f9de093ecfaeb7e139a1dae704eca315e047
robert.sadretdinov <robert.sadretdinov@rtlabs.ru>
2017-06-16 14:34:43 +0300

    actually we dont security here

--- src/main/java/com/tenshun/agss/Application.java
+++ src/main/java/com/tenshun/agss/Application.java
@@ -2,10 +2,12 @@ package com.tenshun.agss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/tenshun/agss/rest/Gateway.java
+++ src/main/java/com/tenshun/agss/rest/Gateway.java
@@ -1,7 +1,17 @@
 package com.tenshun.agss.rest;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class Gateway {
+
+    @GetMapping("/")
+    public String test(){
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+    }
 }
